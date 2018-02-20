5098644ba129584e13b5883d7a95ade9cc5ebdc0
Kim Maida <kim.maida@auth0.com>
2018-02-20 15:05:29 -0500

    Add security (to test production)

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ const cors = require('cors');
 
 // App
 const app = express();
+
+function reqSec(req, res, next) {
+  // HTTP Strict Transport Security (HSTS)
+  // Enforces HTTPS across the entire app
+  // While nginx does a redirect, HSTS redirects
+  // before anything is sent to the server
+  res.setHeader('Strict-Transport-Security', 'max-age=630720');
+  // Defend against Cross Site Scripting (XSS)
+  // This is when a malicious entity injects scripts
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  // Require iFrame sources to come from the same origin
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  // Content Security Policy
+  // Helpful in preventing XSS to ensure scripts only come
+  // from approved origins
+  res.setHeader("Content-Security-Policy", "script-src 'self'");
+  // Send the request on with security headers
+  return next();
+}
+app.use(reqSec);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
